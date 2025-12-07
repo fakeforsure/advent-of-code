1a89377d289c74739d7a93401fa336c9f5de5ab8
Luc Bassaler-Merpillat <luc_bassaler-merpillat@sfu.ca>
2025-12-06 18:09:55 -0800

    Switched file language

--- 2025/day01/Main.pde
+++
@@ -1,5 +0,0 @@
-// AOC Day 1
-// By: Fakeforsure
-// On: December 3, 2025
-
-// TODO the code, too tired right now :c
---
+++ 2025/day01/day1.py
@@ -0,0 +1,8 @@
+# Advent Code - Day 1
+# By: http://github.com/fakeforsure
+# On: December 3, 2025
+
+# Imports
+import pathlib
+folder_of_code = pathlib.Path(__file__).parent.resolve()
+file = open(f"{folder_of_code}/adventcode_day1.txt") ### Load file
